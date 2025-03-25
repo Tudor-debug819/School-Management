8654b51b5baae26dd7b8eb52e334a0e698af8caa
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2025-03-25 18:32:22 +0200

    add error message

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,24 +2,31 @@ import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms'
 import { AuthService } from './auth.service';
 import { RouterModule } from '@angular/router';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import { login } from './auth.actions';
+import { Observable } from 'rxjs';
+import { selectAuthError } from './auth.selectors';
+import { NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [FormsModule, RouterModule],
+  imports: [FormsModule, RouterModule, NgIf, CommonModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
   email = '';
   password = '';
+  errorMessage$: Observable<string | null>;
 
-  constructor(private authService: AuthService, private store: Store) { }
+  constructor(private authService: AuthService, private store: Store) {
+    this.errorMessage$ = this.store.pipe(select(selectAuthError));
+  }
 
   login() {
-    this.store.dispatch(login({email: this.email,password: this.password}))
+    this.store.dispatch(login({ email: this.email, password: this.password }))
   }
 
 
--- src/app/auth/auth.selectors.ts
+++ src/app/auth/auth.selectors.ts
@@ -1,9 +1,11 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { AuthState } from './auth.state';
- 
+
 export const selectAuthState = createFeatureSelector<AuthState>('auth');
- 
+
 export const selectIsAuthenticated = createSelector(
   selectAuthState,
   (state: AuthState) => state.isAuthenticated
 );
+
+export const selectAuthError = createSelector(selectAuthState, (state: AuthState) => state.error);
