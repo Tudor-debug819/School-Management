bdf4a76c8ab9559974c229981109e9a93be35cc9
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2025-03-26 14:23:59 +0200

    Create course.actions.ts

---
+++ src/app/admin-dashboard/course.actions.ts
@@ -0,0 +1,17 @@
+import { createAction, props } from "@ngrx/store";
+import { Course } from "./course.model";
+
+//Load
+export const loadCourses = createAction('[Course] Load Courses');
+export const loadCoursesSuccess = createAction('[Course] Load Courses Success', props<{ courses: Course[] }>());
+export const loadCoursesFailure = createAction('[Course] Load Courses Failure', props<{ error: string }>());
+ 
+// Add
+export const addCourse = createAction('[Course] Add Course', props<{ course: Course }>());
+export const addCourseSuccess = createAction('[Course] Add Course Success', props<{ course: Course }>());
+export const addCourseFailure = createAction('[Course] Add Course Failure', props<{ error: string }>());
+ 
+// Delete
+export const deleteCourse = createAction('[Course] Delete Course', props<{ courseId: string }>());
+export const deleteCourseSuccess = createAction('[Course] Delete Course Success', props<{ courseId: string }>());
+export const deleteCourseFailure = createAction('[Course] Delete Course Failure', props<{ error: string }>());
